f82a25e99c5dc1c5e6f267b93b2223d3d3dd6043
LG-tianyuan <320550662@qq.com>
2022-11-30 17:13:05 +0800

    update azure_lrc(+1)

--- sim_code/pic_sim_py/different_code/Azure_LRC.py
+++ sim_code/pic_sim_py/different_code/Azure_LRC.py
@@ -39,35 +39,29 @@ class Azure_LRC(Code_Placement):
     def generate_best_placement(self):
         cluster_id = 0
         self.b = self.d - 1
-        remain_tails = []
-        for group in self.stripe_information:
-            cur_group_len = len(group)
-            if self.b >= cur_group_len:
-                if len(self.best_placement['raw_information']) > 0:
-                    last_cluster_blk_num = len(self.best_placement['raw_information'][cluster_id - 1].return_all_blocks())
-                    # merge groups into least clusters if possible, but do not split any group
-                    if self.b - last_cluster_blk_num >= cur_group_len:
-                        for block in group:
-                            self.best_placement['raw_information'][cluster_id - 1].add_new_block(block, self.block_to_groupnumber[block])
-                            self.best_placement['block_map_clusternumber'][block] = cluster_id - 1
-                    else:
-                        new_cluster = cluster(cluster_id, self.b)
-                        for block in group:
-                            new_cluster.add_new_block(block, self.block_to_groupnumber[block])
-                            self.best_placement['block_map_clusternumber'][block] = cluster_id
-                        self.best_placement['raw_information'].append(new_cluster)
-                        cluster_id += 1
-                else:
-                    new_cluster = cluster(cluster_id, self.b)
+        theta = math.floor(self.g / self.r)
+        remain_local_groups = []
+        ii = 0
+        for i in range(self.l):
+            if self.r + 1 <= self.b:
+                new_cluster = cluster(cluster_id, self.g + theta)
+                for j in range(ii, ii + theta):
+                    if j >= self.l:
+                        break
+                    group = self.stripe_information[j]
                     for block in group:
                         new_cluster.add_new_block(block, self.block_to_groupnumber[block])
                         self.best_placement['block_map_clusternumber'][block] = cluster_id
-                    self.best_placement['raw_information'].append(new_cluster)
-                    cluster_id += 1
+                self.best_placement['raw_information'].append(new_cluster)
+                cluster_id += 1
+                ii += theta
+                if ii >= self.l:
+                    break
             else:
+                group = self.stripe_information[i]
                 for block_id in range(0, len(group), self.b):
-                    if block_id + self.b >= len(group):
-                        remain_tails.append(group[block_id:])
+                    if block_id + self.b > len(group):
+                        remain_local_groups.append(group[block_id:])
                     else:
                         new_cluster = cluster(cluster_id, self.b)
                         for block in group[block_id:block_id+self.b]:
@@ -75,25 +69,106 @@ class Azure_LRC(Code_Placement):
                             self.best_placement['block_map_clusternumber'][block] = cluster_id
                         self.best_placement['raw_information'].append(new_cluster)
                         cluster_id += 1
+        new_cluster = cluster(cluster_id, self.b)
+        global_group = self.stripe_information[self.l]
+        for block in global_group:
+            new_cluster.add_new_block(block, self.block_to_groupnumber[block])
+            self.best_placement['block_map_clusternumber'][block] = cluster_id
+        self.best_placement['raw_information'].append(new_cluster)
+        g_cluster_id = cluster_id
+        cluster_id += 1
         # if there is any tail, try to merge most tails into the least clusters
         # for Azure-LRC, sometimes it can help reduce the repair cost of global parity blocks
         # for example, (n, l, r) = (15, 10, 5)
-        if len(remain_tails) > 0:
-            for tail in remain_tails:
-                cur_tail_len = len(tail)
-                last_cluster_blk_num = len(self.best_placement['raw_information'][cluster_id - 1].return_all_blocks())
-                # merge tails into the least clusters if possible, but do not split any tail
-                if self.b - last_cluster_blk_num >= cur_tail_len:
-                    for block in tail:
-                        self.best_placement['raw_information'][cluster_id - 1].add_new_block(block, self.block_to_groupnumber[block])
-                        self.best_placement['block_map_clusternumber'][block] = cluster_id - 1
+        l_ = len(remain_local_groups)
+        if l_ > 0:
+            theta = -1
+            if self.k % self.r == 0:
+                m = len(remain_local_groups[0])
+                if m == 1:  # (14, 9, 3)
+                    for i in range(l_):
+                        self.best_placement['raw_information'][g_cluster_id].set_upperbound(self.g + l_)
+                        for block in remain_local_groups[i]:
+                            self.best_placement['raw_information'][g_cluster_id].add_new_block(block,
+                                                                                                 self.block_to_groupnumber[
+                                                                                                     block])
+                            self.best_placement['block_map_clusternumber'][block] = g_cluster_id
+                else:  # (17, 12, 4)
+                    theta = math.floor(self.g / (m - 1))
+                    for i in range(0, l_, theta):
+                        new_cluster = cluster(cluster_id, self.g + theta)
+                        for j in range(i, i + theta):
+                            if j >= l_:
+                                break
+                            for block in remain_local_groups[j]:
+                                new_cluster.add_new_block(block, self.block_to_groupnumber[block])
+                                self.best_placement['block_map_clusternumber'][block] = cluster_id
+                        self.best_placement['raw_information'].append(new_cluster)
+                        cluster_id += 1
+            else:
+                if (self.r + 1) % self.b != 0:
+                    m = len(remain_local_groups[0])
+                    if m == 1:  # (13, 8, 3)
+                        for i in range(self.l - 1):
+                            self.best_placement['raw_information'][g_cluster_id].set_upperbound(self.g + self.l - 1)
+                            for block in remain_local_groups[i]:
+                                self.best_placement['raw_information'][g_cluster_id].add_new_block(block,
+                                                                                                     self.block_to_groupnumber[
+                                                                                                         block])
+                                self.best_placement['block_map_clusternumber'][block] = g_cluster_id
+                    else:   # (15, 10, 4)
+                        theta = math.floor(self.g / (m - 1))
+                        for i in range(0, self.l - 1, theta):
+                            new_cluster = cluster(cluster_id, self.g + theta)
+                            for j in range(i, i + theta):
+                                if j >= self.l - 1:
+                                    break
+                                for block in remain_local_groups[j]:
+                                    new_cluster.add_new_block(block, self.block_to_groupnumber[block])
+                                    self.best_placement['block_map_clusternumber'][block] = cluster_id
+                            self.best_placement['raw_information'].append(new_cluster)
+                            cluster_id += 1
+                    if self.l == l_:
+                        m_ = len(remain_local_groups[-1])
+                        index = math.floor((self.l - 1) / theta) * theta
+                        if m_ == 1:
+                            if m == 1:  # (20, 15, 6)
+                                self.best_placement['raw_information'][g_cluster_id].set_upperbound(self.g + self.l)
+                            else:   # (16, 11, 4)
+                                self.best_placement['raw_information'][g_cluster_id].set_upperbound(self.g + 1)
+                            for block in remain_local_groups[-1]:
+                                self.best_placement['raw_information'][g_cluster_id].add_new_block(block,
+                                                                                                   self.block_to_groupnumber[
+                                                                                                       block])
+                                self.best_placement['block_map_clusternumber'][block] = g_cluster_id
+                        elif m != 1 and self.l - 1 != index and (l_ - 1 - index) * m + m_ <= self.g + (l_ - index):  # (17,11,5)
+                            self.best_placement['raw_information'][cluster_id - 1].set_upperbound(self.g + l_ - index)
+                            for block in remain_local_groups[-1]:
+                                self.best_placement['raw_information'][cluster_id - 1].add_new_block(block, self.block_to_groupnumber[block])
+                                self.best_placement['block_map_clusternumber'][block] = cluster_id - 1
+                        else:  # (15, 9, 4), (14, 9, 4)
+                            new_cluster = cluster(cluster_id, self.b)
+                            for block in remain_local_groups[-1]:
+                                new_cluster.add_new_block(block, self.block_to_groupnumber[block])
+                                self.best_placement['block_map_clusternumber'][block] = cluster_id
+                            self.best_placement['raw_information'].append(new_cluster)
+                            cluster_id += 1
                 else:
-                    new_cluster = cluster(cluster_id, self.b)
-                    for block in tail:
-                        new_cluster.add_new_block(block, self.block_to_groupnumber[block])
-                        self.best_placement['block_map_clusternumber'][block] = cluster_id
-                    self.best_placement['raw_information'].append(new_cluster)
-                    cluster_id += 1
+                    m_ = len(remain_local_groups[-1])
+                    if m_ == 1:  # (18, 13, 5)
+                        self.best_placement['raw_information'][g_cluster_id].set_upperbound(self.g + 1)
+                        for block in remain_local_groups[-1]:
+                            self.best_placement['raw_information'][g_cluster_id].add_new_block(block,
+                                                                                               self.block_to_groupnumber[
+                                                                                                   block])
+                            self.best_placement['block_map_clusternumber'][block] = g_cluster_id
+                    else:  # (19, 14, 5)
+                        new_cluster = cluster(cluster_id, self.b)
+                        for block in remain_local_groups[-1]:
+                            new_cluster.add_new_block(block, self.block_to_groupnumber[block])
+                            self.best_placement['block_map_clusternumber'][block] = cluster_id
+                        self.best_placement['raw_information'].append(new_cluster)
+                        cluster_id += 1
         assert self.check_cluster_information(self.best_placement)
 
     def calculate_distance(self):
@@ -114,8 +189,8 @@ class Azure_LRC(Code_Placement):
 
 if __name__ == '__main__':
     azure_lrc = Azure_LRC()
-    n = 18
-    k = 12
+    n = 17
+    k = 11
     r = 5
     k, l, g, r = azure_lrc.nkr_to_klgr(n, k, r)
     print('Azure-LRC(k,l,g,r)', k, l, g, r)
--- sim_code/pic_sim_py/different_code/Azure_LRC_1.py
+++ sim_code/pic_sim_py/different_code/Azure_LRC_1.py
@@ -36,37 +36,30 @@ class Azure_LRC_1(Code_Placement):
     def generate_best_placement(self):
         cluster_id = 0
         self.b = self.d - 1
-        remain_tails = []
-        for group in self.stripe_information:
-            cur_group_len = len(group)
-            if self.b >= cur_group_len:
-                if len(self.best_placement['raw_information']) > 0:
-                    last_cluster_blk_num = len(
-                        self.best_placement['raw_information'][cluster_id - 1].return_all_blocks())
-                    if self.b - last_cluster_blk_num >= cur_group_len:
-                        for block in group:
-                            self.best_placement['raw_information'][cluster_id - 1].add_new_block(block,
-                                                                                                 self.block_to_groupnumber[
-                                                                                                     block])
-                            self.best_placement['block_map_clusternumber'][block] = cluster_id - 1
-                    else:
-                        new_cluster = cluster(cluster_id, self.b)
-                        for block in group:
-                            new_cluster.add_new_block(block, self.block_to_groupnumber[block])
-                            self.best_placement['block_map_clusternumber'][block] = cluster_id
-                        self.best_placement['raw_information'].append(new_cluster)
-                        cluster_id += 1
-                else:
-                    new_cluster = cluster(cluster_id, self.b)
+        self.l -= 1
+        theta = math.floor(self.g / self.r)
+        remain_local_groups = []
+        ii = 0
+        for i in range(self.l):
+            if self.r + 1 <= self.b:
+                new_cluster = cluster(cluster_id, self.g + theta)
+                for j in range(ii, ii + theta):
+                    if j >= self.l:
+                        break
+                    group = self.stripe_information[j]
                     for block in group:
                         new_cluster.add_new_block(block, self.block_to_groupnumber[block])
                         self.best_placement['block_map_clusternumber'][block] = cluster_id
-                    self.best_placement['raw_information'].append(new_cluster)
-                    cluster_id += 1
+                self.best_placement['raw_information'].append(new_cluster)
+                cluster_id += 1
+                ii += theta
+                if ii >= self.l:
+                    break
             else:
+                group = self.stripe_information[i]
                 for block_id in range(0, len(group), self.b):
-                    if block_id + self.b >= len(group):
-                        remain_tails.append(group[block_id:])
+                    if block_id + self.b > len(group):
+                        remain_local_groups.append(group[block_id:])
                     else:
                         new_cluster = cluster(cluster_id, self.b)
                         for block in group[block_id:block_id + self.b]:
@@ -74,25 +67,107 @@ class Azure_LRC_1(Code_Placement):
                             self.best_placement['block_map_clusternumber'][block] = cluster_id
                         self.best_placement['raw_information'].append(new_cluster)
                         cluster_id += 1
-        # for Azure-LRC+1, merging tail or not meets the same repair cost, but to meet the least clusters, we can try
-        if len(remain_tails) > 0:
-            for tail in remain_tails:
-                cur_tail_len = len(tail)
-                last_cluster_blk_num = len(self.best_placement['raw_information'][cluster_id - 1].return_all_blocks())
-                # merge tails into the least clusters if possible, but do not split any tail
-                if self.b - last_cluster_blk_num >= cur_tail_len:
-                    for block in tail:
-                        self.best_placement['raw_information'][cluster_id - 1].add_new_block(block,
-                                                                                             self.block_to_groupnumber[
-                                                                                                 block])
-                        self.best_placement['block_map_clusternumber'][block] = cluster_id - 1
+        new_cluster = cluster(cluster_id, self.b)
+        global_group = self.stripe_information[self.l]
+        for block in global_group:
+            new_cluster.add_new_block(block, self.block_to_groupnumber[block])
+            self.best_placement['block_map_clusternumber'][block] = cluster_id
+        self.best_placement['raw_information'].append(new_cluster)
+        g_cluster_id = cluster_id
+        cluster_id += 1
+        #
+        l_ = len(remain_local_groups)
+        if l_ > 0:
+            theta = -1
+            if self.k % self.r == 0:
+                m = len(remain_local_groups[0])
+                if m == 1:  # (15, 9, 3)
+                    for i in range(l_):
+                        self.best_placement['raw_information'][g_cluster_id].set_upperbound(self.g + 1 + l_)
+                        for block in remain_local_groups[i]:
+                            self.best_placement['raw_information'][g_cluster_id].add_new_block(block,
+                                                                                               self.block_to_groupnumber[
+                                                                                                   block])
+                            self.best_placement['block_map_clusternumber'][block] = g_cluster_id
+                else:  # (18, 12, 4)
+                    theta = math.floor(self.g / (m - 1))
+                    for i in range(0, l_, theta):
+                        new_cluster = cluster(cluster_id, self.g + theta)
+                        for j in range(i, i + theta):
+                            if j >= l_:
+                                break
+                            for block in remain_local_groups[j]:
+                                new_cluster.add_new_block(block, self.block_to_groupnumber[block])
+                                self.best_placement['block_map_clusternumber'][block] = cluster_id
+                        self.best_placement['raw_information'].append(new_cluster)
+                        cluster_id += 1
+            else:
+                if (self.r + 1) % self.b != 0:
+                    m = len(remain_local_groups[0])
+                    if m == 1:  # (14, 8, 3)
+                        for i in range(self.l - 1):
+                            self.best_placement['raw_information'][g_cluster_id].set_upperbound(self.g + self.l)
+                            for block in remain_local_groups[i]:
+                                self.best_placement['raw_information'][g_cluster_id].add_new_block(block,
+                                                                                                   self.block_to_groupnumber[
+                                                                                                       block])
+                                self.best_placement['block_map_clusternumber'][block] = g_cluster_id
+                    else:  # (16, 10, 4)
+                        theta = math.floor(self.g / (m - 1))
+                        for i in range(0, self.l - 1, theta):
+                            new_cluster = cluster(cluster_id, self.g + theta)
+                            for j in range(i, i + theta):
+                                if j >= self.l - 1:
+                                    break
+                                for block in remain_local_groups[j]:
+                                    new_cluster.add_new_block(block, self.block_to_groupnumber[block])
+                                    self.best_placement['block_map_clusternumber'][block] = cluster_id
+                            self.best_placement['raw_information'].append(new_cluster)
+                            cluster_id += 1
+                    if self.l == l_:
+                        m_ = len(remain_local_groups[-1])
+                        index = math.floor((self.l - 1) / theta) * theta
+                        if m_ == 1:
+                            if m == 1:  # (21, 15, 6)
+                                self.best_placement['raw_information'][g_cluster_id].set_upperbound(self.g + 1 + self.l)
+                            else:  # (17, 11, 4)
+                                self.best_placement['raw_information'][g_cluster_id].set_upperbound(self.g + 2)
+                            for block in remain_local_groups[-1]:
+                                self.best_placement['raw_information'][g_cluster_id].add_new_block(block,
+                                                                                                   self.block_to_groupnumber[
+                                                                                                       block])
+                                self.best_placement['block_map_clusternumber'][block] = g_cluster_id
+                        elif m != 1 and self.l - 1 != index and (l_ - 1 - index) * m + m_ <= self.g + (
+                                l_ - index):  # (18,11,5)
+                            self.best_placement['raw_information'][cluster_id - 1].set_upperbound(self.g + l_ - index)
+                            for block in remain_local_groups[-1]:
+                                self.best_placement['raw_information'][cluster_id - 1].add_new_block(block,
+                                                                                                     self.block_to_groupnumber[
+                                                                                                         block])
+                                self.best_placement['block_map_clusternumber'][block] = cluster_id - 1
+                        else:  # (16, 9, 4), (15, 9, 4)
+                            new_cluster = cluster(cluster_id, self.b)
+                            for block in remain_local_groups[-1]:
+                                new_cluster.add_new_block(block, self.block_to_groupnumber[block])
+                                self.best_placement['block_map_clusternumber'][block] = cluster_id
+                            self.best_placement['raw_information'].append(new_cluster)
+                            cluster_id += 1
                 else:
-                    new_cluster = cluster(cluster_id, self.b)
-                    for block in tail:
-                        new_cluster.add_new_block(block, self.block_to_groupnumber[block])
-                        self.best_placement['block_map_clusternumber'][block] = cluster_id
-                    self.best_placement['raw_information'].append(new_cluster)
-                    cluster_id += 1
+                    m_ = len(remain_local_groups[-1])
+                    if m_ == 1:  # (19, 13, 5)
+                        self.best_placement['raw_information'][g_cluster_id].set_upperbound(self.g + 2)
+                        for block in remain_local_groups[-1]:
+                            self.best_placement['raw_information'][g_cluster_id].add_new_block(block,
+                                                                                               self.block_to_groupnumber[
+                                                                                                   block])
+                            self.best_placement['block_map_clusternumber'][block] = g_cluster_id
+                    else:  # (20, 14, 5)
+                        new_cluster = cluster(cluster_id, self.b)
+                        for block in remain_local_groups[-1]:
+                            new_cluster.add_new_block(block, self.block_to_groupnumber[block])
+                            self.best_placement['block_map_clusternumber'][block] = cluster_id
+                        self.best_placement['raw_information'].append(new_cluster)
+                        cluster_id += 1
         assert self.check_cluster_information(self.best_placement)
 
     def calculate_distance(self):
@@ -115,10 +190,10 @@ class Azure_LRC_1(Code_Placement):
 if __name__ == '__main__':
     azure_lrc_1 = Azure_LRC_1()
     n = 18
-    k = 12
+    k = 11
     r = 5
     k, l, g, r = azure_lrc_1.nkr_to_klgr(n, k, r)
-    print('Azure-LRC(k,l,g,r)', k, l, g, r)
+    print('Azure-LRC+1(k,l,g,r)', k, l, g, r)
     print(azure_lrc_1.return_DRC_NRC(k, l, g, r, "flat", 10, True))
     print(azure_lrc_1.return_DRC_NRC(k, l, g, r, "random", 10, True))
     print(azure_lrc_1.return_DRC_NRC(k, l, g, r, "best", 10, True))
